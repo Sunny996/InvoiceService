dba758652ab25ea653715e18abd7d2a5c14e4a02
Sunny996 <satlaakash@gmail.com>
2022-04-29 08:38:57 +0530

    REFACTOR: Correctd the variable name

--- src/main/java/com/invoice/InvoiceGenerator.java
+++ src/main/java/com/invoice/InvoiceGenerator.java
@@ -1,13 +1,13 @@
 package com.invoice;
 
 public class InvoiceGenerator {
-    private static final double MINMUM_COST_PER_KILOMETER = 10.0;
+    private static final double MINIMUM_COST_PER_KILOMETER = 10.0;
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_FARE = 5;
 
     public double calculateFare(double distance, int time) {
 
-        double totalFare = distance * MINMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
         if (totalFare < MINIMUM_FARE)
             return MINIMUM_FARE;
         return totalFare;
